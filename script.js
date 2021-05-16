490c325810427a1fed6b1f184557e5132a9de3cd
trybe-chess-club <84277344+trybe-chess-club@users.noreply.github.com>
2021-05-16 14:27:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //adiciona frase do Club
 const frasesDoClub = {
     autor: 'Rafael Luiz',
-    frase: "'Se quiser ir rápido, pode ir sozinho. Mas se você quer ir longe, vamos juntos!'",
+    frase: "'Se você quer ir rápido, pode ir sozinho. Mas se você quer ir longe, vamos todos juntos!'",
 }
 // acessa elemento da frase
 const fraseDoClub = document.querySelector('#frases-do-club');
